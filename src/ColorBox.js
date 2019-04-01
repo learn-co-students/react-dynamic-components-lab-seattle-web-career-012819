2babc5ca6969ec1db2a05cb52a35d782d9ff0e8f
halented <hallie.dunn1@gmail.com>
2019-04-01 11:37:30 -0700

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,10 +4,12 @@ export default class ColorBox extends Component {
   
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
       </div>
     )
   }
   
 }
+
+//got rid of style={{opacity: 2}} from the div tags because i assume we are going to be fiddling with the opacity so it shouldn't be hard coded to one value every time we render the colorbox? idk
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
-// add Comment component here
+import React, { Component } from 'react';
+import BlogPost from './BlogPost'
+
+class Comment extends React.Component {
+    render () {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        )
+    }
+}
+
+export default Comment;
